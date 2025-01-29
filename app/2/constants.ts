9173b49ce62f5bfcf6a4357827b1e9e0de056c13
Guilherme Rodz <gui.rodz.dev@gmail.com>
2025-01-29 03:21:34 -0300

    Add UI components and styling with Radix UI, Tailwind, and theme support

---
+++ app/2/constants.ts
@@ -0,0 +1,6 @@
+import { cn } from "@/lib/utils";
+
+export const CN_SMOOTH_BORDER = cn("border-[.75px] border-border");
+export const CN_SMOOTH_SHADOW = cn(
+  "[box-shadow:0_0_0_1px_#e3e1de66,0_1px_2px_#5f4a2e14,0_4px_6px_#5f4a2e0a,0_40px_40px_-24px_#684b2514] dark:[box-shadow:none]",
+);
